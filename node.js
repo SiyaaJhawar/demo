522664fc760cd19eaa5a44f2d3f07c9c60891ed7
SiyaaJhawar <119915532+SiyaaJhawar@users.noreply.github.com>
2023-02-13 10:14:19 +0530

    Create node.js

---
+++ node.js
@@ -0,0 +1,32 @@
+const fs = require("fs");
+const jwt = require("jsonwebtoken");
+
+async function authenticate() {
+  
+  const appId = 292855;
+  const apiUrl = "https://api.github.com";
+
+
+  const encryptedPrivateKey = fs.readFileSync("private.key.enc", "utf-8");
+
+
+  const privateKey = await decrypt(encryptedPrivateKey);
+
+
+  const header = {
+    alg: "RS256",
+    typ: "JWT"
+  };
+  const payload = {
+    iat: Math.floor(Date.now() / 1000),
+    exp: Math.floor(Date.now() / 1000) + (10 * 60), // expires in 10 minutes
+    iss: appId
+  };
+  const jwtToken = jwt.sign(payload, privateKey, { algorithm: "RS256", header });
+
+
+
+authenticate();
+
+
+
