d7600f688aeb6c945cf684e7c65b64627cba0314
SiyaaJhawar <119915532+SiyaaJhawar@users.noreply.github.com>
2023-02-13 11:17:45 +0530

    Create appinstallation.js

---
+++ appinstallation.js
@@ -0,0 +1,35 @@
+
+const axios = require("axios");
+
+async function createInstallationToken(appId, privateKey, INSTALLATION_ID) {
+  const jwt = require("jsonwebtoken");
+
+  const now = Math.floor(Date.now() / 1000);
+  const payload = {
+    iat: now,
+    exp: now + (10 * 60),
+    iss: appId
+  };
+  const token = jwt.sign(payload, privateKey, { algorithm: "RS256" });
+
+  const response = await axios.post(`https://api.github.com/app/installations/${installationId}/access_tokens`, {}, {
+    headers: {
+      Accept: "application/vnd.github+json",
+      Authorization: `Bearer ${token}`
+    }
+  });
+  return response.data.token;
+}
+
+createInstallationToken("APP_ID", "PRIVATE_KEY", "INSTALLATION_ID").then(token => {
+  console.log(token);
+});
+
+
+
+
+
+
+
+
+
