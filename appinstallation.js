135d2232b6dfa27aaef407204c475cec09c171fb
SiyaaJhawar <119915532+SiyaaJhawar@users.noreply.github.com>
2023-02-13 12:18:13 +0530

    Update appinstallation.js

--- appinstallation.js
+++ appinstallation.js
@@ -1,33 +1,24 @@
-
 const axios = require("axios");
-const appId = process.env.APP_ID;
-const installationId = process.env.INSTALLATION_ID;
-const privateKey = process.env.PRIVATE_KEY;
 
-async function createInstallationToken(appId, privateKey, installation_Id) {
-  const jwt = require("jsonwebtoken");
+async function createInstallationToken(installationId) {
+  try {
+    const response = await axios.post(
+      `https://api.github.com/app/installations/34148902/access_tokens`,
+      {},
+      {
+        headers: {
+          Accept: "application/vnd.github+json",
+          Authorization: `Bearer $secrets.GITHUB_TOKEN`
+        }
+      }
+    );
 
-  const now = Math.floor(Date.now() / 1000);
-  const payload = {
-    iat: now,
-    exp: now + (10 * 60),
-    iss: 292855
-  };
-  const token = jwt.sign(payload, privateKey, { algorithm: "RS256" });
-
-  const response = await axios.post(`https://api.github.com/app/installations/ process.env.INSTALLATION_ID/access_tokens`, {}, {
-    headers: {
-      Accept: "application/vnd.github+json",
-      Authorization: `Bearer ${token}`
-    }
-  });
-  return response.data.token;
+    return response.data.token;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
 }
-
-createInstallationToken("APP_ID", "PRIVATE_KEY", "INSTALLATION_ID").then(token => {
-  console.log(token);
-});
-
 
 
 
