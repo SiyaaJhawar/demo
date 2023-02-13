accce539e24094a4866c724897ab303fa7cb5ca6
SiyaaJhawar <119915532+SiyaaJhawar@users.noreply.github.com>
2023-02-13 12:39:44 +0530

    Update appinstallation.js

--- appinstallation.js
+++ appinstallation.js
@@ -1,24 +1,38 @@
-const axios = require("axios");
+const axios = require('axios');
+const jwt = require('jsonwebtoken');
 
-async function createInstallationToken(installationId) {
+const createInstallationToken = async () => {
+  const app_id = process.env.APP_ID;
+  const installation_id = process.env.INSTALLATION_ID;
+  const private_key = process.env.PRIVATE_KEY;
+
+  const payload = {
+    iat: Math.floor(Date.now() / 1000),
+    exp: Math.floor(Date.now() / 1000) + (10 * 60),
+    iss: app_id,
+  };
+  
+  const jwtToken = jwt.sign(payload, private_key, { algorithm: 'RS256' });
+  
   try {
-    const response = await axios.post(
-      `https://api.github.com/app/installations/34148902/access_tokens`,
-      {},
-      {
-        headers: {
-          Accept: "application/vnd.github+json",
-          Authorization: `Bearer $secrets.GITHUB_TOKEN`
-        }
-      }
-    );
-
+    const response = await axios.post(`https://api.github.com/app/installations/installation_id/access_tokens`, {}, {
+      headers: {
+        Authorization: `Bearer ${jwtToken}`,
+        Accept: 'application/vnd.github+json',
+      },
+    });
     return response.data.token;
   } catch (error) {
     console.error(error);
-    throw error;
   }
-}
+};
+
+
+
+
+
+
+
 
 
 
