204f200dbda28d16678d34dd8b39f54907b46cbf
SiyaaJhawar <119915532+SiyaaJhawar@users.noreply.github.com>
2023-02-20 14:29:47 +0530

    Update appinstallation.js

--- appinstallation.js
+++ appinstallation.js
@@ -17,7 +17,7 @@ async function authenticate() {
     iss: appId
   };
   const jwtToken = jwt.sign(payload, privateKey, { algorithm: "RS256", header });
-  process.env.ACCESS_TOKEN = jwtToken;
+
 }
 
 authenticate();
