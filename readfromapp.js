b4d3be6374705c2e7223cb1d2bd823c01973b8c9
SiyaaJhawar <119915532+SiyaaJhawar@users.noreply.github.com>
2023-02-20 11:43:50 +0530

    Update readfromapp.js

--- readfromapp.js
+++ readfromapp.js
@@ -9,6 +9,6 @@ async function getRepository(owner, repo, token) {
   return response.data;
 }
 
-getRepository("SiyaaJhawar", "demo", "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").then(repository => {
+getRepository("SiyaaJhawar", "demo", "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").then(repository => {
   console.log(repository);
 });
