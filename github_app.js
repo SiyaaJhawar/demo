cf65447aedae21cdb1eb4a0236cb65a84bc16209
SiyaaJhawar <119915532+SiyaaJhawar@users.noreply.github.com>
2023-02-22 10:49:31 +0530

    Create github_app.js

---
+++ github_app.js
@@ -0,0 +1,19 @@
+require('dotenv').config()
+const { Octokit } = require('@octokit/core')
+const jwt = require('jsonwebtoken')
+
+const appId = process.env.APP_ID
+const privateKey = require('fs').readFileSync(process.env.PRIVATE_KEY_PATH)
+
+const now = Math.floor(Date.now() / 1000)
+const payload = {
+  iat: now,
+  exp: now + (10 * 60), 
+  iss: appId
+}
+const token = jwt.sign(payload, privateKey, { algorithm: 'RS256' })
+
+const octokit = new Octokit({ auth: token })
+octokit.request('GET /user').then(response => {
+  console.log(response.data.login)
+})
