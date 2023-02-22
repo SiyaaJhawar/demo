40774344b67f6fee6297fffb97568d036b46ae92
SiyaaJhawar <119915532+SiyaaJhawar@users.noreply.github.com>
2023-02-22 11:55:29 +0530

    Update github_app.js

--- github_app.js
+++ github_app.js
@@ -14,6 +14,6 @@ const payload = {
 const token = jwt.sign(payload, privateKey, { algorithm: 'RS256' })
 
 const octokit = new Octokit({ auth: token })
-octokit.request('GET /SiyaaJhawar/demo').then(response => {
+octokit.request('GET /user').then(response => {
   console.log(response.data.login)
 })
