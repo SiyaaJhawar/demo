2e155f4a10e99b6bc883eaa7c6886a05230c23b0
SiyaaJhawar <119915532+SiyaaJhawar@users.noreply.github.com>
2023-02-22 15:47:19 +0530

    Create issue2.js

---
+++ issue2.js
@@ -0,0 +1,17 @@
+const octokit = new Octokit({ auth: token })
+
+octokit.request('POST /repos/:owner/:repo/issues', {
+  owner: 'SiyaaJhawar',
+  repo: 'demo',
+  title: 'Hi',
+  body: 'Issue created by github app'
+}).then(response => {
+  console.log(response.data)
+}).catch(error => {
+  console.error(error)
+})
+
+
+
+
+
