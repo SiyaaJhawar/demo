41e22bbc4d71a79b27a6fb0f3249945485360b41
SiyaaJhawar <119915532+SiyaaJhawar@users.noreply.github.com>
2023-02-22 17:49:37 +0530

    Update github_app.js

--- github_app.js
+++ github_app.js
@@ -13,7 +13,7 @@ const payload = {
 }
 const token = jwt.sign(payload, privateKey, { algorithm: 'RS256' })
 
-const octokit = new Octokit({ auth:username: 'SiyaaJhawar',password: 'Siyaa@12345'})
+const octokit = new Octokit({ auth:token,username: 'SiyaaJhawar',password: 'Siyaa@12345'})
 octokit.request('GET /user').then(response => {
   console.log(response.data.login)
 })
