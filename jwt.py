b24b3d739d7ae522b1d0e429e1d4819086f5bac7
SiyaaJhawar <119915532+SiyaaJhawar@users.noreply.github.com>
2023-02-23 10:30:33 +0530

    Create jwt.py

---
+++ jwt.py
@@ -0,0 +1,32 @@
+
+import jwt
+import time 
+import sys
+
+if len(sys.argv) > 1:
+    pem = sys.argv[1]
+else:
+    pem = input("Enter path of private PEM file: ")    
+
+if len(sys.argv) > 2:
+    app_id = sys.argv[2]
+else:
+    app_id = input("Enter your APP ID: ") 
+
+with open(pem, 'rb') as pem_file:
+    signing_key = jwt.jwk_from_pem(pem_file.read())
+    
+payload = {
+  
+    'iat': int(time.time()),
+  
+    'exp': int(time.time()) + 600, 
+
+    'iss': app_id 
+}
+    
+
+jwt_instance = jwt.JWT()
+encoded_jwt = jwt_instance.encode(payload, signing_key, alg='RS256')
+     
+print(f"JWT:  ", encoded_jwt)
