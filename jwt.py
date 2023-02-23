b5fe5fcf82daa2a62d699332325cf1fccd030d22
SiyaaJhawar <119915532+SiyaaJhawar@users.noreply.github.com>
2023-02-23 11:44:58 +0530

    Update jwt.py

--- jwt.py
+++ jwt.py
@@ -7,15 +7,7 @@ import os
 if len(sys.argv) > 1:
     pem = sys.argv[1]
 else:
-   pem = os.path.expanduser("siyaajhawar-app.2023-02-22.private-key")
-pem_abs_path = os.path.abspath(pem)
-
-
-with open(pem_abs_path, 'r') as f:
-    private_key = f.read()
-
-
-print(private_key)   
+  pem = input("~\Downloads\siyaajhawar-app.2023-02-22.private-key")    
 
 if len(sys.argv) > 2:
     app_id = sys.argv[2]
