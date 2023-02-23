f7f4167092d4e7b26090bf989ceb833318bfca43
SiyaaJhawar <119915532+SiyaaJhawar@users.noreply.github.com>
2023-02-23 15:09:15 +0530

    Update jwt.py

--- jwt.py
+++ jwt.py
@@ -8,7 +8,7 @@ import sys
 if len(sys.argv) > 1:
     pem = sys.argv[1]
 else:
-    pem = input("demo/siyaajhawar-app.2023-02-22.private-key")    
+    pem = input("~/Downloads/siyaajhawar-app.2023-02-22.private-key.pem")    
 
 if len(sys.argv) > 2:
     app_id = sys.argv[2]
