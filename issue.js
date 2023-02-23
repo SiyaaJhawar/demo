a33985bdcee262009fd8fb9d637e3ff711bff65d
SiyaaJhawar <119915532+SiyaaJhawar@users.noreply.github.com>
2023-02-23 15:32:39 +0530

    Update issue.js

--- issue.js
+++ issue.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const jwt = require('jsonwebtoken');
 
-const privateKeyPath = 'C:\Users\swgu\Downloads\siyaajhawar-app.2023-02-22.private-key';
+const privateKeyPath = 'siyaajhawar-app.2023-02-22.private-key';
 const privateKey = fs.readFileSync(privateKeyPath);
 
 const payload = { data: 'example data' };
