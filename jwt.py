94c27f8dea862ad031b71782aa59429b069d680d
SiyaaJhawar <119915532+SiyaaJhawar@users.noreply.github.com>
2023-02-23 12:31:07 +0530

    Update jwt.py

--- jwt.py
+++ jwt.py
@@ -1,25 +1,35 @@
+
+
+
 import jwt
-import time
+import time 
 import sys
 
+if len(sys.argv) > 1:
+    pem = sys.argv[1]
+else:
+    pem = input("~/Downloads/siyaajhawar-app.2023-02-22.private-key ")    
 
-with open('siyaajhawar-app.2023-02-22.private-key', 'r') as f:
-    private_key = f.read()
+if len(sys.argv) > 2:
+    app_id = sys.argv[2]
+else:
+    app_id = input("292855 ") 
 
-# Define the payload for the JWT
+with open(pem, 'rb') as pem_file:
+    signing_key = jwt.jwk_from_pem(pem_file.read())
+    
 payload = {
-      'iss': '292855',
-    'iat': int(time.time()), 
-    'exp': int(time.time()) + 600,
-   
+ 
+    'iat': int(time.time()),
+    # JWT expiration time (10 minutes maximum)
+    'exp': int(time.time()) + 600, 
+    # GitHub App's identifier
+    'iss': app_id
 }
-
+    
+# Create JWT
 jwt_instance = jwt.JWT()
-
 encoded_jwt = jwt_instance.encode(payload, signing_key, alg='RS256')
      
 print(f"JWT:  ", encoded_jwt)
-
-
-
 
