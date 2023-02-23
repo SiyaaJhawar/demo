cf15f184fa16a4c90c668b21513d496da4f42230
SiyaaJhawar <119915532+SiyaaJhawar@users.noreply.github.com>
2023-02-23 12:12:46 +0530

    Update jwt.py

--- jwt.py
+++ jwt.py
@@ -7,16 +7,17 @@ with open('siyaajhawar-app.2023-02-22.private-key', 'r') as f:
 
 # Define the payload for the JWT
 payload = {
+      'iss': '292855',
     'iat': int(time.time()), 
     'exp': int(time.time()) + 600,
-    'sub': 'user123'  
+   
 }
 
 
 jwt_encoded = jwt.encode(payload, private_key, algorithm='RS256')
 
 
-print(jwt_encoded)
+print(jwt_encoded.decode())
 
 
 
