a9e6e0bb35fd44ca7e574abb04c38dad5319c971
SiyaaJhawar <119915532+SiyaaJhawar@users.noreply.github.com>
2023-02-23 18:27:03 +0530

    Update jwt.py

--- jwt.py
+++ jwt.py
@@ -17,7 +17,8 @@ if len(sys.argv) > 2:
 else:
     app_id = "292855" 
 
-
+ with open(siyaajhawar-app.2023-02-22.private-key, 'rb') as pem_file:
+    signing_key = jwt.jwk_from_siyaajhawar-app.2023-02-22.private-key(pem_file.read())
    
    
     
@@ -33,7 +34,7 @@ payload = {
     
 # Create JWT
 jwt_instance = jwt.JWT()
-encoded_jwt = jwt_instance.encode(payload, private_key, alg='RS256')
+encoded_jwt = jwt_instance.encode(payload,  signing_key, alg='RS256')
      
 print(f"JWT:  ", encoded_jwt)
 sys.exit()
