6c15033d7ede0a82e9c13ee617f410e564674d41
SiyaaJhawar <119915532+SiyaaJhawar@users.noreply.github.com>
2023-02-23 15:31:19 +0530

    Update issue.js

--- issue.js
+++ issue.js
@@ -1,29 +1,15 @@
-const axios = require('axios');
 const fs = require('fs');
 const jwt = require('jsonwebtoken');
 
+const privateKeyPath = 'C:\Users\swgu\Downloads\siyaajhawar-app.2023-02-22.private-key';
+const privateKey = fs.readFileSync(privateKeyPath);
 
-const privateKey = fs.readFileSync('siyaajhawar-app.2023-02-22.private-key');
-
-
-const appId = 292855;
-
-const installationId =34148902 ;
+const payload = { data: 'example data' };
+const options = { algorithm: 'RS256' };
 
 
-const now = Math.floor(Date.now() / 1000);
+const encodedJWT = jwt.sign(payload, privateKey, options);
 
-
-const payload = {
-  iat: now,
-  exp: now + 60,
-  iss: appId
-};
-
-
-const encoded_jwt = jwt.sign(payload, privateKey, { algorithm: 'RS256' });
-//console.log(token)
-console.log(encoded_jwt)
-
+console.log(encodedJWT);
 
 
