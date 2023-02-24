0686760801ed674613804029327d1f059bb80c8e
SiyaaJhawar <119915532+SiyaaJhawar@users.noreply.github.com>
2023-02-24 17:16:34 +0530

    Update gen_jwt.py

--- gen_jwt.py
+++ gen_jwt.py
@@ -1,19 +1,20 @@
 import jwt
 import datetime
 
+app_id = '292855'
+
 # Read the PEM file
 with open('siyaajhawar-app.2023-02-22.private-key.pem', 'r') as f:
     private_key = f.read()
 
 # Set the payload
 payload = {
-    'sub': '1234567890',
-    'name': 'John Doe',
     'iat': datetime.datetime.utcnow(),
     'exp': datetime.datetime.utcnow() + datetime.timedelta(days=1)
+    'iss': app_id
 }
 
 # Generate the token
 token = jwt.encode(payload, private_key, algorithm='RS256')
 
-print(token)
+print('%s' % token)
