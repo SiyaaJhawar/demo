603d245980a8ee19851354f4e61b519f3756ea31
SiyaaJhawar <119915532+SiyaaJhawar@users.noreply.github.com>
2023-02-24 23:53:50 +0530

    Update gen_jwt.py

--- gen_jwt.py
+++ gen_jwt.py
@@ -1,3 +1,4 @@
+'''
 import jwt
 import datetime
 
@@ -16,5 +17,25 @@ payload = {
 
 # Generate the token
 token = jwt.encode(payload, private_key, algorithm='RS256')
-
 print('%s' % token)
+'''
+import jwt
+from datetime import datetime, timedelta
+
+# Define the payload of the token
+payload = {
+    'iat': datetime.datetime.utcnow(),
+    'exp': datetime.datetime.utcnow() + datetime.timedelta(days=1)
+    'iss':  '292855' # token expiration time
+}
+
+# Load the private key from the PEM file
+with open('siyaajhawar-app.2023-02-22.private-key.pem', 'r') as f:
+    private_key = f.read()
+
+print(private_key)
+
+# Generate the JWT token using the private key
+jwt_token = jwt.encode(payload, private_key, algorithm='RS256')
+
+print(jwt_token)
