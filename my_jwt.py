6b93e63eba9847f6ee59c2ea4892ea912cc21dcd
SiyaaJhawar <119915532+SiyaaJhawar@users.noreply.github.com>
2023-02-24 16:27:30 +0530

    Update my_jwt.py

--- my_jwt.py
+++ my_jwt.py
@@ -13,45 +13,36 @@ from cryptography.hazmat.primitives.asymmetric import rsa
 
 
 if len(sys.argv) > 1:
-    pem = sys.argv[1]
-
+  pem_file = sys.argv[1]
 else:
   pem_file = "siyaajhawar-app.2023-02-22.private-key.pem"
-   print(pem_file)
+print(pem_file)
 if len(sys.argv) > 2:
-    app_id = sys.argv[2]
+  app_id = sys.argv[2]
 else:
-    app_id = "292855" 
+  app_id = "292855" 
+print(app_id)
     
 pem_file = "siyaajhawar-app.2023-02-22.private-key.pem"
 
 with open(pem_file, 'rb') as pem:
-     pem_content = pem.read()
-     signing_key = jwk.JWK.from_pem(pem_content)
-    
-
-   
-    
+  pem_content = pem.read()
+  signing_key = jwk.JWK.from_pem(pem_content)
     
 payload = {
- 
-    'iat': int(time.time()),
-    # JWT expiration time (10 minutes maximum)
-    'exp': int(time.time()) + 600, 
-    # GitHub App's identifier
-    'iss': app_id
-  
+  'iat': int(time.time()),
+  # JWT expiration time (10 minutes maximum)
+  'exp': int(time.time()) + 600, 
+  # GitHub App's identifier
+  'iss': app_id
 }
     
 # Create JWT
-
 private_key = rsa.generate_private_key(
-    public_exponent=65537,
-    key_size=2048,
-    backend=default_backend()
+  public_exponent=65537,
+  key_size=2048,
+  backend=default_backend()
 )
-
-
 
 token = jwt.encode(payload, private_key, algorithm='RS256')
 print(f"JWT:  ", token)
@@ -60,6 +51,3 @@ print(f"JWT:  ", token)
 public_key = private_key.public_key()
 decoded_token = jwt.decode(token, public_key, algorithms=['RS256'])
 print(decoded_token)
-
-
-
